ec865b5ba043dcf353ed7b5d48aecdbb2a6cdd66
Joseph Hoppert <jehoppert@gmail.com>
2021-05-08 20:16:17 +0200

    type checks and BMI calculator

--- day-1-working with variables/main.py
+++ day-1-working with variables/main.py
@@ -1,20 +1,18 @@
-#coding challenge 1.1
-#print out some items
+#coding challenge 1.1 - printing
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
-#coding challenge 1.2
-#fixed errors
+#coding challenge 1.2 - fdebugging practice
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
-#coding challenge 1.3
+#coding challenge 1.3 - input function
 print(len(input("What is your name?"))) #print the len of the input name
 
-#coding challenge 1.4
+#coding challenge 1.4 - variables
 a = input("a:")
 b = input("b:")
 a,b = b,a #swap a & b
--- day-2-data types-string manipulation/main.py
+++ day-2-data types-string manipulation/main.py
@@ -1,1 +1,12 @@
-#
+"""
+# 2.1 Coding Challenge - data types
+two_digit_number = input("Enter a two digit number: ")
+#print(type(two_digit_number))
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+#2.2 Coding Challenge - BMI Calculator
+height = float(input("Enter your height in m: "))
+weight = int(input("Enter your weight in kg: "))
+bmi = weight / height**2
+print(int(bmi))
+"""
