e1e381f7b589411d500df79dcec443a81c76e581
Joseph Hoppert <jehoppert@gmail.com>
2021-05-13 18:29:07 +0200

    working on day 8 project

--- day-8-functions and inputs/main.py
+++ day-8-functions and inputs/main.py
@@ -1,1 +1,26 @@
-#8.1 Coding Challenge - 
+#8.1 Coding Challenge - paint area calculator
+import math
+def paint_calc(height, width, cover):
+  #determine number of paint cans to purchase
+  num_of_cans = math.ceil((height * width) / cover)
+  print(f"You will need {num_of_cans} of paint")
+
+h = int(input("Height of wall: "))
+w = int(input("Width of wall: "))
+coverage = 5 #5m^2
+paint_calc(height = h, width = w, cover = coverage)
+
+#8.2 Coding Challenge - prime number check
+def prime_check(number):
+  #check to see if a number is prime
+  prime = True
+  for i in range(2,number):
+    if number % i == 0:
+      prime = False
+  if prime:
+    print("It's a prime number")
+  else:
+    print("It's not a prime number")
+
+n = int(input("Check this number: "))
+prime_check(number = n)
