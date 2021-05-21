a064158515f881b4abab9722f3425ac8d7a94d66
Joseph Hoppert <jehoppert@gmail.com>
2021-05-21 17:39:45 +0200

    OOP quiz game

---
+++ day-17-benefits of OOP/quiz_game.py
@@ -0,0 +1,16 @@
+from quiz_game_question_model import Question
+from quiz_game_data import question_data
+from quiz_game_quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+  new_question = Question(question["question"], question["correct_answer"])
+  question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+  quiz.next_question()
+
+print("You completed the quiz!")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
